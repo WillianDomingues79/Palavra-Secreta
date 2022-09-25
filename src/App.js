a4952eb65bc997f2e570cb65bb559ddbe0760eb8
WillDomingues <willian.domingues3@gmail.com>
2022-09-25 16:01:29 -0300

    Adicionado Telas do jogo e estilização inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css'
+import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+
+import { useCallback, useEffect, useState } from 'react'
+
+import { wordsList } from './data/words'
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' }
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  console.log(words)
+
   return (
     <div className="App">
-      <h2>Palavra Secreta</h2>
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   )
 }
